c4e9387e50c9e0cd3c601b1155a3a1e787eedaf0
19DennisBrown <densmoney12@gmail.com>
2024-05-14 18:34:28 +0300

    Dictionary operations

--- Dictionaries/dictionary.py
+++ Dictionaries/dictionary.py
@@ -19,3 +19,19 @@ newDict = dict(
   writer='Daniel'
 )
 print(f"The newly created dictionary is : {newDict}")
+# Adding new key:value pair
+newDict['size']='A4'
+# Getting the value of the provided key
+print(f"Value of name is : {newDict.get('name')}")
+# Getting all the keys
+print(f"These are the keys : {newDict.keys()}")
+# Getting all the values
+print(f"These are the values : {newDict.values()}")
+# Getting all the items
+print(f"These are the items : {newDict.items()}")
+
+# Checking if key exists
+if 'size' in newDict:
+  print(f"It exists!! It has a value of {newDict.get('size')}")
+else:
+  print(f"It does not exist")
