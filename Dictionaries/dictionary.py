25313b0ba1dc1b10873b460f8ded90dcd35cfd0d
19DennisBrown <densmoney12@gmail.com>
2024-05-14 18:21:56 +0300

    Dictionaries

---
+++ Dictionaries/dictionary.py
@@ -0,0 +1,21 @@
+
+
+thisDict = {
+  'brand':'Ford',
+  'model':'Mustang',
+  'year':1964,
+  'colors': ['red', 'blue', 'black'],
+  'electric?': False
+}
+
+# print(thisDict)
+# print(thisDict['brand'])
+# print("The length is: ",len(thisDict))
+
+"""Creating a new a dictionary using dict keyword"""
+newDict = dict(
+  name='Books',
+  genre='Comic',
+  writer='Daniel'
+)
+print(f"The newly created dictionary is : {newDict}")
