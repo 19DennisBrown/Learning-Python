d1e1d2cc7f5af5521b6390d46398886950da0fa9
19DennisBrown <densmoney12@gmail.com>
2024-05-25 16:47:05 +0300

    functions

--- Functions/functions.py
+++ Functions/functions.py
@@ -19,3 +19,25 @@ argsInfo('Johan', 'Lev', 'Dafe')
 def kwargsInfo(**children):
   print(f"last name is {children['lname']}")
 kwargsInfo(fname='me', lname='you')
+
+# default parameter value
+def defParam(country = "Norway"):
+  print(f"I love {country}")
+defParam("Kenya")
+
+# Passing list as argument
+def funcFood(food):
+  for c in food:
+    print(c)
+fruits = ['Apple', 'Banana', 'Cherry']
+funcFood(fruits)
+
+
+# Return values
+def funcReturn(c):
+  return 6*c
+
+# Pass stmt
+def funcPass():
+  pass
+
