99e359247a63557bdc1f931ed0fbcb83bd72129e
19DennisBrown <densmoney12@gmail.com>
2024-07-30 08:41:36 +0300

    Python math

---
+++ Python_Math/python_math.py
@@ -0,0 +1,31 @@
+"""
+Python has a set of built-in math functions, including an extensive math module.
+"""
+
+x = min(5, 10, 25) #Finds the minimum value
+y = max(5, 10, 25) #Finds the maximum value
+
+print(x)
+print(y)
+
+c = abs(-5.45) #gets the positive value of given integer
+print(c)
+
+pov = pow(3, 2) #finds the square of 3
+print(f"The square of 3 is {pov}")
+
+
+import math
+
+root = math.sqrt(81)
+print(f"The square root of 81 is {root}")
+
+top = math.ceil(1.4)
+bot = math.floor(1.4)
+
+print(f"Ceiling value is {top}")
+print(f"Floor value is {bot}")
+
+
+pie = math.pi
+print(f"Pie is {pie}")
